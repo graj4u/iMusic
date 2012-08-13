7761ee4edb5d366b7291471455c8a0bf57fb9306
Gaurav <gauravraj@me.com>
2012-08-13 14:40:11 +0530

    Initial Commit

---
+++ iMusic/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  iMusic
+//
+//  Created by Gaurav Raj on 8/13/12.
+//  Copyright (c) 2012 Gaurav Raj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
